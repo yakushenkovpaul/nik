fd209b1d9595e54850cab67d73018fada783ccfd
SpeedupGuru <yakushenkovpaul@gmail.com>
2023-05-07 02:25:09 +0700

    da

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ let start = (variables['start']) ? parseInt(variables['start']) : 1;
 let limit = (variables['limit']) ? parseInt(variables['limit']) : 500;
 let timeout = (variables['timeout']) ? parseInt(variables['timeout']) : 2500;
 
-const parse_link = 'https://www.99.co/id/jual/rumah/bali?harga_maks=1,25mily&hlmn=';
+let parse_link = (variables['url']) ? variables['url'] : '';
+
+if(!parse_link)
+{
+	//https://www.99.co/id/jual/rumah/bali?harga_maks=1,25mily&hlmn=
+	console.log('Please provide url to parse');
+	return;
+}
 
 mongoConnect(() => {
 	parse(parse_link, start, limit, timeout, (data) => {
