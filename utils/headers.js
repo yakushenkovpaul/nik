a760f503eecd04f98f84adc01bec03949c9ce50b
SpeedupGuru <yakushenkovpaul@gmail.com>
2023-05-07 17:15:04 +0700

    da

--- utils/headers.js
+++ utils/headers.js
@@ -1,5 +1,5 @@
 const headers = {
-	'Cookie':'cf_chl_2=c46484c526b137f; cf_clearance=4ECjOugbypUWeocfBRsKIc6T12SyeJPgka.AHWHFtNA-1683454081-0-250;',
+	'Cookie':'cf_chl_2=a3e81cb45497a60; cf_clearance=KkwgfhWPIb6.MRF8xsCXPUmdy8mKPHytlop7YIyhd24-1683454359-0-250;',
 	'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
 };
 
