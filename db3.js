f65c304eaef4d98d97d0674287156809dc698c78
SpeedupGuru <yakushenkovpaul@gmail.com>
2023-06-03 13:56:21 +0300

    da

--- db3.js
+++ db3.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const { subDays, format } = require('date-fns');
 const MongoClient = require('mongodb').MongoClient;
 
 loadAndProcessProducts();
@@ -20,22 +22,39 @@ async function loadAndProcessProducts() {
 
         let num = 0;
 				let totalFeatures = {};
+				let totalProjectFeatures = {};
 
         for(let page = 1; page <= 1; page++) {
             const products = await collection.find({}).skip((page - 1) * batchSize).limit(batchSize).toArray();
 
             for(const product of products) {
-                // process each product here
+								normalizeString(product.data.features, totalFeatures);
+								normalizeString(product.data.projectFeatures, totalProjectFeatures);
 
-								features = normalizeString(product.data.features, totalFeatures);
+								console.log(product.data.basicInformation);
 
-                console.log(product.data.id);
-                console.log(features);
+								let preparedBasicInformation = normalizeBasicInformation(product.data.basicInformation);
+								console.log(preparedBasicInformation);
                 num++;
             }
         }
 
-        console.log(totalFeatures);
+				fs.writeFile('totalFeatures.json', JSON.stringify(totalFeatures, null, 2), (err) => {
+					if (err) {
+							console.error(err);
+					} else {
+							console.log('totalFeatures written to file');
+					}
+				});
+
+				fs.writeFile('totalProjectFeatures.json', JSON.stringify(totalProjectFeatures, null, 2), (err) => {
+					if (err) {
+							console.error(err);
+					} else {
+							console.log('totalProjectFeatures written to file');
+					}
+				});
+
         console.log(`Total number of processed products: ${num}`);
 
     } catch (err) {
@@ -46,19 +65,50 @@ async function loadAndProcessProducts() {
 }
 
 
-function normalizeString(str, array) {
+function normalizeString(str, array = {}) {
 	str = str.toLowerCase();
 	str = str.replace(/\s/g, '_');
 	str = str.replace(/[^a-z_,]/g, '');
 
 	const phrases = str.split(',');
-	const obj = {};
 
 	for(let phrase of phrases) {
 			phrase = phrase.trim();
 			phrase = phrase.replace(/^_+|_+$/g, '');
-			if(!)
-			obj[phrase] = 1;
+			if(!array[phrase]) {
+				array[phrase] = 1;
+			}
+	}
+
+	return array;
+}
+
+function normalizeBasicInformation(str) {
+	const pairs = str.split(';');
+	const obj = {};
+
+	for(let pair of pairs) {
+			pair = pair.trim().split(':');
+			if(pair.length === 2) {
+					let [key, value] = pair;
+					key = key.toLowerCase().replace(/\s/g, '_').replace(/[^a-z_]/g, '');
+					value = value.trim();
+
+					if (key === 'date_listed') {
+							let date = new Date(value);
+							value = date.toLocaleDateString('en-GB');
+					}
+
+					if (key === 'updated') {
+						if(value.includes('day ago')) {
+								const daysAgo = Number(value.split(' ')[0]);
+								let date = subDays(new Date(), daysAgo);
+								value = format(date, 'dd/MM/yyyy');
+						}
+				}
+					
+					obj[key] = value;
+			}
 	}
 
 	return obj;
